c132f66c5a2e960ba9381197367dd812f5631d12
Kevin Li <kevin.li195@hotmail.com>
2015-12-27 11:05:47 +0800

    Touches up previous commit and adds more to MovingObject abstract class.

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -10,11 +10,50 @@ public abstract class MovingObject : MonoBehaviour {
     private Rigidbody2D rb2d;
     private float inverseMoveTime;
 
-	private protected void Start () {
-	
+	protected void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f/moveTime;
 	}
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit) {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+        if(hit.transform == null) {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void AttemptMove<T>(int xDir, int yDir) where T : Component {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null) {
+            return;
+        }
+
+        T hitComponent = hit.transform.GetComponent<T>();
+        if (!canMove && hitComponent != null) {
+            OnCantMove(hitComponent);
+        }
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end) {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon) {
+            Vector3 newPosition = Vector3.MoveTowards(rb2d.position, end, inverseMoveTime * Time.deltaTime);
+            rb2d.MovePosition(newPosition);
+            sqrRemainingDistance = (end - newPosition).sqrMagnitude;
+            yield return null;
+        }
+    }
 	
-	void Update () {
-	
-	}
+    protected abstract void OnCantMove<T> (T component)
+        where T : Component;
 }
